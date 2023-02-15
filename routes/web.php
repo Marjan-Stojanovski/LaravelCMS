65b24aa85e5ff3b37f47e7fbe697e70d8caf2f08
Marjan Stojanovski <stojanovskimarjan4@gmail.com>
2023-02-15 20:18:29 +0100

    Crud Users

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,10 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::middleware(('web','auth')->prefix('dashboard')->group(function() {
     Route::get('users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
+    Route::get('users', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
+    Route::post('users', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}', [\App\Http\Controllers\UserController::class, 'show'])->name('users.show');
+    Route::get('/users/{user}/edit', [\App\Http\Controllers\UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [\App\Http\Controllers\UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [\App\Http\Controllers\UserController::class, 'destroy'])->name('users.destroy');
 });
