208e09f68d10c6fbc09294e95f50abbf0e5fd25d
Marjan Stojanovski <stojanovskimarjan4@gmail.com>
2023-02-15 20:05:32 +0100

    Install dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(('web','auth')->prefix('dashboard')->group(function() {
+    Route::get('users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+
+});
