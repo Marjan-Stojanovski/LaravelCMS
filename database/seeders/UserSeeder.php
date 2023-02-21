cd05c8555bd105ae0deb22889cdd69c3016707f9
Marjan Stojanovski <stojanovskimarjan4@gmail.com>
2023-02-21 15:25:14 +0100

    Implemented roles and countries

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,20 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'Marjan Stojanovski',
+            'email' => 'stojanovskim@yahoo.com',
+            'password' => bcrypt('temp12345'),
+            'role_id' => rand(1,3),
+            'country_id' => rand(1,233)
+        ]);
+
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.mk',
+            'password' => bcrypt('temp12345'),
+            'role_id' => 1,
+            'country_id' => rand(1,223)
+        ]);
     }
 }
