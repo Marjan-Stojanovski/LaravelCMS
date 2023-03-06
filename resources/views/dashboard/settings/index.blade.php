2c50326af4bfe9c70808db0e4d430f6d20825295
Marjan Stojanovski <stojanovskimarjan4@gmail.com>
2023-03-06 21:30:39 +0100

    Settings CRUD in the making

--- resources/views/dashboard/settings/index.blade.php
+++ resources/views/dashboard/settings/index.blade.php
@@ -25,13 +25,13 @@
                     <div class="text-center">
                         <br>
                         <img src="/assets/img/settings/medium/{{$settings->image}}" alt="slika"
-                             class="img-responsive rounded-5 mb-3 mb-lg-0 shadow-lg" style="width: 400px; position: ">
+                             class="img-responsive rounded-5 mb-3 mb-lg-0 shadow-lg" style="width: 400px">
                     </div>
                     <div class="card-header">
                         <h5>{{$settings->title}}</h5>
-                        <small class="text-muted">{{$settings->email}}</small>
+                        <small class="text-muted">{{$settings->mainurl}}</small>
                         <br>
-                        <small class="text-muted">{{$settings->address}}</small>
+                        <small class="text-muted">Последна промена: {{$settings->created_at->diffForHumans()}}</small>
                     </div>
 
                 </div>
@@ -51,35 +51,46 @@
                         </thead>
                         <tbody>
                         <tr class="align-middle" style="height: 50px">
+                            <td>Twitter</td>
+                            <td>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->twitter}}</div>
+                            </td>
+                        </tr>
+                        <tr class="align-middle" style="height: 50px">
                             <td>Facebook</td>
                             <td>
-                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->facebook}}
-
-                                </div>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->facebook}}</div>
                             </td>
                         </tr>
                         <tr class="align-middle" style="height: 50px">
                             <td>Skype</td>
                             <td>
-                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->skype}}
-
-                                </div>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->skype}}</div>
                             </td>
                         </tr>
-                        <tr class="align-middle" style="height: 50px">
-                            <td>Twitter</td>
-                            <td>
-                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->twitter}}
 
-                                </div>
-                            </td>
-                        </tr>
                         <tr class="align-middle" style="height: 50px">
                             <td>LinkedIn</td>
                             <td>
-                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->linkedin}}
-
-                                </div>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->linkedin}}</div>
+                            </td>
+                        </tr>
+                        <tr class="align-middle" style="height: 50px">
+                            <td>Youtube</td>
+                            <td>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->youtube}}</div>
+                            </td>
+                        </tr>
+                        <tr class="align-middle" style="height: 50px">
+                            <td>Flickr</td>
+                            <td>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->flickr}}</div>
+                            </td>
+                        </tr>
+                        <tr class="align-middle" style="height: 50px">
+                            <td>Pinterest</td>
+                            <td>
+                                <div class="fs-6 d-flex justify-content-between align-items-center">{{$settings->pinterest}}</div>
                             </td>
                         </tr>
                         </tbody>
@@ -87,6 +98,53 @@
                 </div>
             </div>
         </div>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card table-card table-wrap">
+                    <div class="card table-card table-nowrap">
+                        <div class="card-header">
+                            <h5 class="mb-0">Линкови</h5>
+                        </div>
+                        <table class="table">
+                            <tbody>
+                            <tr class="align-middle" style="height: 50px">
+                                <td>
+                                    <div class="fs-6 d-flex justify-content-between align-items-center"><h6>Адреса</h6></div>
+                                    <small class="text-muted">{{$settings->address}}</small>
+                                </td>
+                            </tr>
+                            <tr class="align-middle" style="height: 50px">
+                                <td>
+                                    <div class="fs-6 d-flex justify-content-between align-items-center"><h6>Телефон</h6></div>
+                                    <small class="text-muted">{{$settings->phone}}</small>
+                                </td>
+                            </tr>
+                            <tr class="align-middle" style="height: 50px">
+                                <td>
+                                    <div class="fs-6 d-flex justify-content-between align-items-center"><h6>Email</h6></div>
+                                    <small class="text-muted">{{$settings->email}}</small>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-6 mb-3 mb-lg-5">
+                <div class="card overflow-hidden">
+                    <div class="card-header">
+                        <h5 class="mb-0">Опис на компанијата</h5>
+                    </div>
+                    <div class="card-body" style="min-height: 5rem;">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 
 @endsection
